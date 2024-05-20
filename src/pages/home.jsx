4c05714c14e0f5643d391bf14b3df6c13b9794d5
sholom perman <sholomperman@sholoms-MacBook-Pro.local>
2024-05-20 19:27:44 -0400

    made bg fixeds

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -21,8 +21,8 @@ export function Home() {
   return (
     <>
       <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
-        <div className="absolute top-0 h-full w-full bg-[url('/img/background-2.jpg')] bg-cover bg-center" />
-        <div className="absolute top-0 h-full w-full bg-black/60 bg-cover bg-center" />
+        <div style={{ zIndex: -1 }} className="fixed top-0 h-full w-full bg-[url('/img/background-2.jpg')] bg-cover bg-center" />
+        <div style={{ zIndex: -1 }} className="fixed top-0 h-full w-full bg-black/50  bg-cover bg-center" />
         <div className="max-w-8xl container relative mx-auto">
           <div className="flex flex-wrap items-center">
             <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
@@ -42,9 +42,9 @@ export function Home() {
           </div>
         </div>
       </div>
-      <section className="-mt-32 bg-white px-4 pb-20 pt-4">
+      <section className="bg-white px-4 pb-20 pt-4"> {/* -mt-32 removed from here and added to 47:000 */}
         <div className="container mx-auto">
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2">
+          <div className="grid -mt-32  grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2">
             {featuresData.map(({ color, title, icon, link }) => (
               <FeatureCard
                 key={title}
