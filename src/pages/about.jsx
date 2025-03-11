ff306e2048259925272363c21fe414568bbd6c78
sholom perman <sholomperman@Sholoms-MacBook-Pro.local>
2025-03-10 20:11:33 -0400

    added a new nav to prices and more options

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,38 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   Typography,
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
 } from "@material-tailwind/react";
+
 import { PageTitle } from "@/widgets/layout";
-import {  servicesData } from "@/data";
+import { services } from "@/data";
 
 const About = () => {
-  
+  const [position, setPosition] = useState(0)
   return (
-    <section id="Precios" className="relative py-12 px-4 md:scroll-mt-0 scroll-mt-[160px]">
+  <section id="Precios" className="relative py-12 px-4 md:scroll-mt-0 scroll-mt-[160px]">
     <div className="container mx-auto">
       <PageTitle section="PRECIOS" />
-      <div className="mx-auto mt-8 mb-0 grid max-w-5xl grid-cols-2 gap-16 md:grid-cols-2 lg:grid-cols-2">
-        {servicesData.map(({ title, price, icon }, index) => (
-          
-          <Card
-            key={title}
-            color="transparent"
-            shadow={false}
-            className="text-center"
-          >
-            <div className="mx-auto mb-6 grid h-14 w-14 place-items-center center rounded-full bg-blue-gray-900 shadow-lg shadow-gray-500/20">
-              {icon}
+      <Tabs value={services[1].title}>
+        <TabsHeader className="bg-bg_secondary">
+          {services.map((i, index) => (
+            <Tab className="h-100" onClick={()=>setPosition(index)} key={index} value={i.title}>
+              {i.title}
+            </Tab>
+          ))}
+        </TabsHeader>
+      </Tabs>
+        {
+            <>
+              <Typography className="text-bg_secondary mb-2 text-md text-center font-semibold m-8" >{services[position].type}</Typography>
+              <div className="mx-auto mt-8 mb-0 grid max-w-5xl grid-cols-2 gap-16 md:grid-cols-2">
+                {
+                services[position].items.map(({ title, price, icon }) => (
+                    <Card
+                      key={title}
+                      color="transparent"
+                      shadow={false}
+                      className="text-center"
+                    >
+                      <div className="mx-auto mb-6 grid h-14 w-14 place-items-center center rounded-full bg-blue-gray-900 shadow-lg shadow-gray-500/20">
+                        {icon}
+                      </div>
+                      <Typography variant="h3" className="text-txt_primary txt_p_font mb-2 text-xl font-semibold">
+                        {title}
+                      </Typography>
+                      <Typography variant="h5" className="text-txt_secondary txt_s_font opacity-70 font-normal text-lg">
+                        ${price}
+                      </Typography>
+                    </Card>
+                  ))
+                }
             </div>
-            <Typography variant="h3" className="text-txt_primary txt_p_font mb-2 text-xl font-semibold">
-              {title}
-            </Typography>
-            <Typography variant="h5" className="text-txt_secondary txt_s_font opacity-70 font-normal text-lg">
-              ${price}
-            </Typography>
-          </Card>
-        ))}
-      </div>
+            </>
+        }
     </div>
   </section>
   )
